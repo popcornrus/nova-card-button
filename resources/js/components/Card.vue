01f22428d9dbbf668cbdb113cc1b7b73a7a54a7b
Mihail Micoliuc <32881606+popcornrus@users.noreply.github.com>
2023-09-26 16:11:38 +0300

    Update Card.vue

--- resources/js/components/Card.vue
+++ resources/js/components/Card.vue
@@ -5,7 +5,7 @@
             <div class="mt-3 flex justify-center">
                 <a
                    class="flex-shrink-0 h-9 px-4 focus:outline-none ring-primary-200 dark:ring-gray-600 focus:ring text-white dark:text-gray-800 inline-flex items-center font-bold shadow rounded focus:outline-none ring-primary-200 dark:ring-gray-600 focus:ring bg-primary-500 hover:bg-primary-400 active:bg-primary-600 text-white dark:text-gray-800 inline-flex items-center font-bold px-4 h-9 text-sm flex-shrink-0 h-9 px-4 focus:outline-none ring-primary-200 dark:ring-gray-600 focus:ring text-white dark:text-gray-800 inline-flex items-center font-bold"
-                   href="http://localhost:32300/nova-api/users/action?action=ban-users&pivotAction=false&search=&filters=W10%3D&trashed=">
+                   href="/nova-api/user-balance-transactions/action?action=delete-profit-statistics&pivotAction=false&search=&filters=W10%3D&trashed=">
                     <span>Reset</span>
                 </a>
             </div>
