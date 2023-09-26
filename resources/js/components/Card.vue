7f3adc107fc5b696fa2fa7d53ed51afefa0bc996
Mihail Micoliuc <32881606+popcornrus@users.noreply.github.com>
2023-09-26 15:52:15 +0300

    Update Card.vue

--- resources/js/components/Card.vue
+++ resources/js/components/Card.vue
@@ -1,12 +1,16 @@
 <template>
-  <Card class="flex flex-col items-center justify-center">
-    <div class="px-3 py-3">
-      <h1 class="text-center text-3xl text-gray-500 font-light">Reset Profit</h1>
-      <div class="flex justify-center"> 
-        <button class="btn btn-danger">Reset</button>
-      </div>
-    </div>
-  </Card>
+    <Card class="flex flex-col items-center justify-center">
+        <div class="px-3 py-3">
+            <h1 class="text-center text-3xl text-gray-500 font-light">Reset Profit</h1>
+            <div class="mt-3 flex justify-center">
+                <a
+                   class="flex-shrink-0 h-9 px-4 focus:outline-none ring-primary-200 dark:ring-gray-600 focus:ring text-white dark:text-gray-800 inline-flex items-center font-bold shadow rounded focus:outline-none ring-primary-200 dark:ring-gray-600 focus:ring bg-primary-500 hover:bg-primary-400 active:bg-primary-600 text-white dark:text-gray-800 inline-flex items-center font-bold px-4 h-9 text-sm flex-shrink-0 h-9 px-4 focus:outline-none ring-primary-200 dark:ring-gray-600 focus:ring text-white dark:text-gray-800 inline-flex items-center font-bold"
+                   href="http://localhost:32300/nova-api/users/action?action=ban-users&pivotAction=false&search=&filters=W10%3D&trashed=">
+                    <span>Reset</span>
+                </a>
+            </div>
+        </div>
+    </Card>
 </template>
 
 <script>
